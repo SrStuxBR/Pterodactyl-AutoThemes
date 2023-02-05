3a7a3b31617e4b652a181bf2e62797122911ba86
SrStuxBR <as787002@gmail.com>
2023-02-05 16:46:06 -0300

    update

--- install.sh
+++ install.sh
@@ -3,20 +3,20 @@
 set -e
 
 ########################################################
-#
-#         Pterodactyl-AutoThemes Installation
-#
-#         Created and maintained by Ferks-FK
-#
-#            Protected by MIT License
-#
+#                                                      #
+#         Instalação do Pterodactyl-AutoThemes         #
+#                                                      #
+#           Criado e mantido por StuxDev               #
+#                                                      #
+#            Protegido pela licença MIT                #
+#                                                      #
 ########################################################
 
-# Get the latest version before running the script #
+# Obtenha a versão mais recente antes de executar o script #
 get_release() {
 curl --silent \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/Ferks-FK/Pterodactyl-AutoThemes/releases/latest |
+  https://api.github.com/repos/SrStuxBR/Pterodactyl-AutoThemes/releases/latest |
   grep '"tag_name":' |
   sed -E 's/.*"([^"]+)".*/\1/'
 }
@@ -24,7 +24,7 @@ curl --silent \
 GITHUB_STATUS_URL="https://www.githubstatus.com"
 SCRIPT_VERSION="$(get_release)"
 
-# Visual Functions #
+# funções visuais #
 print_brake() {
   for ((n = 0; n < $1; n++)); do
     echo -n "#"
@@ -46,30 +46,30 @@ error() {
   echo ""
 }
 
-# Check Sudo #
+# Verificar Sudo #
 if [[ $EUID -ne 0 ]]; then
-  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  echo "* Este script deve ser executado com privilégios de root (sudo)." 1>&2
   exit 1
 fi
 
-# Check Git #
+# Verifique o Git #
 if [ -z "$SCRIPT_VERSION" ]; then
-  error "Could not get the version of the script using GitHub."
-  echo "* Please check on the site below if the 'API Requests' are as normal status."
+  error "Não foi possível obter a versão do script usando o GitHub."
+  echo "* Verifique no site abaixo se as 'solicitações de API' estão com o status normal."
   echo -e "${YELLOW}$(hyperlink "$GITHUB_STATUS_URL")${RESET}"
   exit 1
 fi
 
-# Check Curl #
+# Verificar Curl #
 if ! [ -x "$(command -v curl)" ]; then
-  echo "* curl is required in order for this script to work."
-  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  echo "* curl é necessário para que este script funcione."
+  echo "* instale usando apt (Debian e derivados) ou yum/dnf (CentOS)"
   exit 1
 fi
 
 cancel() {
 echo
-echo -e "* ${RED}Installation Canceled!${RESET}"
+echo -e "* ${RED}Instalação cancelada!${RESET}"
 done=true
 exit 1
 }
@@ -80,59 +80,59 @@ echo
 print_brake 70
 echo "* Pterodactyl-AutoThemes Script @ $SCRIPT_VERSION"
 echo
-echo "* Copyright (C) 2021 - $(date +%Y), Ferks-FK."
-echo "* https://github.com/Ferks-FK/Pterodactyl-AutoThemes"
+echo "* Copyright (C) 2022 - $(date +%Y), SrStuxBR."
+echo "* https://github.com/SrStuxBR/Pterodactyl-AutoThemes"
 echo
-echo "* This script is not associated with the official Pterodactyl Project."
+echo "* Este script não está associado ao Projeto Pterodáctilo oficial."
 print_brake 70
 echo
 
 Backup() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/backup.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/backup.sh)
 }
 
 Dracula() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/Dracula/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/Dracula/build.sh)
 }
 
 Enola() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/Enola/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/Enola/build.sh)
 }
 
 Twilight() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/Twilight/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/Twilight/build.sh)
 }
 
 ZingTheme() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/ZingTheme/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/ZingTheme/build.sh)
 }
 
 FlancoTheme() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/FlancoTheme/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/FlancoTheme/build.sh)
 }
 
 BackgroundVideo() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/BackgroundVideo/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/BackgroundVideo/build.sh)
 }
 
 AnimatedGraphics() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/AnimatedGraphics/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/SrStuxBR/Pterodactyl-AutoThemes/"${SCRIPT_VERSION}"/themes/version1.x/AnimatedGraphics/build.sh)
 }
 
 
 while [ "$done" == false ]; do
   options=(
-    "Restore Panel Backup (Only if you have an installation problem.)"
-    "Install Dracula (Only 1.6.6 and 1.7.0)"
-    "Install Enola (Only 1.6.6 and 1.7.0)"
-    "Install Twilight (Only 1.6.6 and 1.7.0)"
-    "Install Zing Theme (Only 1.6.6 and 1.7.0)"
-    "Install Flanco Theme (Only 1.6.6 and 1.7.0)"
-    "Install Background Video (Only 1.6.6 and 1.7.0)"
-    "Install Animated Graphics (Only 1.6.6 and 1.7.0)"
+    "Restaurar backup do painel (somente se você tiver um problema de instalação.)"
+    "Instale o Drácula (somente 1.6.6 e 1.7.0)"
+    "Instale o Enola (somente 1.6.6 e 1.7.0)"
+    "Instale o Twilight (somente 1.6.6 e 1.7.0)"
+    "Instale o tema Zing (somente 1.6.6 e 1.7.0)"
+    "Instale o Flanco Theme (somente 1.6.6 e 1.7.0)"
+    "Instale o vídeo de fundo (somente 1.6.6 e 1.7.0)"
+    "Instalar gráficos animados (somente 1.6.6 e 1.7.0)"
     
     
-    "Cancel Installation"
+    "Cancelar instalação"
   )
   
   actions=(
@@ -149,7 +149,7 @@ while [ "$done" == false ]; do
     "cancel"
   )
   
-  echo "* Which theme do you want to install?"
+  echo "* Qual tema você deseja instalar?"
   echo
   
   for i in "${!options[@]}"; do
@@ -157,12 +157,12 @@ while [ "$done" == false ]; do
   done
   
   echo
-  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  echo -n "* Entrada 0-$((${#actions[@]} - 1)): "
   read -r action
   
-  [ -z "$action" ] && error "Input is required" && continue
+  [ -z "$action" ] && error "A entrada é necessária" && continue
   
   valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Opção inválida"
   [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && eval "${actions[$action]}"
 done
